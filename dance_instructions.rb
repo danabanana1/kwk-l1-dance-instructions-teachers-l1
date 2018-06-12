8b00376bd311801b363a5ba4810252d1b08d113b
Dana Abdelhalim <dana.abdelhalim18@gmail.com>
2018-06-12 05:24:54 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,10 @@
 
 # Code your methods below
+
+def lower_body;
+  puts "Plant legs far apart, bend knees slightly and keep posture loose. Lift right foot, return right foot to the ground, Finishing with a small skip-step backward."
+def skip_step_left
+  puts "Lower left foot to the ground, Bounce left foot back up slightly, kicking it a few inches back. Left, Right, Left and Left."
+def skip_step_right
+  puts "Lower right foot to the ground, bounce right foot back up slightly, kicking it a few inches back. Right, Left, Right and Right."
+def 
