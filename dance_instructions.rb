1711e07364f4175a9c5f54372f61222cdb237ab9
Dana Abdelhalim <dana.abdelhalim18@gmail.com>
2018-06-12 05:26:24 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,11 +1,13 @@
 
 # Code your methods below
 
-def lower_body;
+def lower_body
   puts "Plant legs far apart, bend knees slightly and keep posture loose. Lift right foot, return right foot to the ground, Finishing with a small skip-step backward."
 def skip_step_left
   puts "Lower left foot to the ground, Bounce left foot back up slightly, kicking it a few inches back. Left, Right, Left and Left."
 def skip_step_right
   puts "Lower right foot to the ground, bounce right foot back up slightly, kicking it a few inches back. Right, Left, Right and Right."
 def bob_the_reins
+  puts "Holding your arms out in front of you, straight and at chest level. Cross your right wrist over your left and hold them together. Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. "
+def lasso
   puts ""
